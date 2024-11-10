88d96aea3e3f1e2d18d3ee584fd2cf1479adf451
Jan-Magnus Monenschein <me@janmagnus.dev>
2024-11-10 13:58:02 +0100

    restructure index page and make ContactsCard.astro a bit better

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -2,24 +2,24 @@ export function trimText(input: string, maxLength: number = 100): string {
   if (input.length <= maxLength) return input;
   return input.substring(0, maxLength - 3) + "...";
 }
-export function getCurrentTimeInItaly(): Date {
+export function getCurrentTime(): Date {
   // Create a date object with the current UTC time
   const now = new Date();
 
-  // Convert the UTC time to Italy's time
-  const offsetItaly = 2; // Italy is in Central European Summer Time (UTC+2), but you might need to adjust this based on Daylight Saving Time
-  now.setHours(now.getUTCHours() + offsetItaly);
+  // Convert the UTC time to Location's time -> date object should handle that on its own
+  const offset = 1;
+  now.setHours(now.getHours());
 
   return now;
 }
 
-export function formatTimeForItaly(date: Date): string {
+export function formatTime(date: Date): string {
   const options: Intl.DateTimeFormatOptions = {
     hour: "numeric",
     minute: "2-digit",
     second: "2-digit",
-    hour12: true, // This will format the time in 12-hour format with AM/PM
-    timeZone: "Europe/Rome",
+    hour12: false, // AM/ PM format
+    timeZone: "Europe/Berlin",
   };
 
   let formattedTime = new Intl.DateTimeFormat("en-US", options).format(date);

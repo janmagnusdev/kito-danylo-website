e9bd4f9508d289da5ff748d8a95f103d8ee42527
Jan-Magnus Monenschein <mail@janmagnus.dev>
2024-11-25 09:25:37 +0100

    feat: finish CoachingCounter.tsx

--- src/components/CoachingCounter.tsx
+++ src/components/CoachingCounter.tsx
@@ -9,7 +9,12 @@ const weekDayCoachingMap = {
   5: 3,
   6: 5,
   7: 0,
-};
+} as const;
+
+const CUTOFF_DATE = DateTime.fromObject({ day: 23, month: 11, year: 2024 });
+const INITIAL_COUNT = 2547;
+const WORKING_START = 19;
+const WORKING_END = 9;
 
 const getCoachingsForDay = (date: DateTime): number => {
   const scaleFactor = 0.7;
@@ -19,40 +24,42 @@ const getCoachingsForDay = (date: DateTime): number => {
   return Math.round((dateBased + monthBased + dayInMonthBased) * scaleFactor);
 };
 
-const simulateCoachings = (startDate: DateTime, endDate: DateTime): number => {
-  let counter = 0;
-
-  const yesterday = endDate.minus({ day: 1 }).set({ hour: 9 });
-  const countDaysBetween = Math.round(
-    yesterday.diff(startDate, ["days"])["days"],
-  );
-  for (let i = 0; i <= countDaysBetween; i++) {
-    counter += getCoachingsForDay(startDate.plus({ days: i }));
-  }
-
+const getProportionalCoachingsForDay = (date: DateTime): number => {
   // proportional increment of last date (today)
-  const wouldBe = getCoachingsForDay(endDate);
+  const wouldBe = getCoachingsForDay(date);
   // 9 is 0, and 19 is full
   const percentage = Math.max(
-    Math.min((endDate.get("hour") - 9) / (19 - 9), 1),
+    Math.min(
+      (date.get("hour") - WORKING_START) / (WORKING_END - WORKING_START),
+      1,
+    ),
     0,
   );
-  const increment = Math.round(percentage * wouldBe);
-  counter += increment;
+  return Math.round(percentage * wouldBe);
+};
+
+const simulateCoachings = (forDate: DateTime): number => {
+  let counter = INITIAL_COUNT;
+
+  const yesterday = forDate.minus({ day: 1 }).set({ hour: 9 });
+  const countDaysBetween = Math.ceil(
+    yesterday.diff(CUTOFF_DATE, ["days"])["days"],
+  );
+  for (let i = 0; i <= countDaysBetween; i++) {
+    counter += getCoachingsForDay(CUTOFF_DATE.plus({ days: i }));
+  }
+
+  counter += getProportionalCoachingsForDay(forDate);
 
   return counter;
 };
 
 const CoachingCounterComponent = () => {
   const timeZone = IANAZone.create("Europe/Berlin");
-  let startDate = DateTime.fromObject(
-    { day: 1, month: 1, year: 2024 },
-    { zone: timeZone },
-  );
 
   let now = DateTime.now().setZone(timeZone);
 
-  const startCoachings = simulateCoachings(startDate, now);
+  const startCoachings = simulateCoachings(now);
   let [counter, _setCounter] = createSignal(startCoachings);
 
   return (
